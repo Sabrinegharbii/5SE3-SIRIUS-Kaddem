036f207e864bc150fdfe766739af01c023655acb
Sabrinegharbii <gharbi.sabrine@esprit.tn>
2023-11-08 13:57:09 +0100

    v2

--- src/test/java/tn/esprit/spring/khaddem/services/EtudiantServiceImplDynamiqueTest.java
+++ src/test/java/tn/esprit/spring/khaddem/services/EtudiantServiceImplDynamiqueTest.java
@@ -1,4 +1,4 @@
-/*package tn.esprit.spring.khaddem.services;
+package tn.esprit.spring.khaddem.services;
 
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -23,7 +23,7 @@ class EtudiantServiceImplDynamiqueTest {
 
     @Order(1)
     @Test
-    public void testAddEtudiant() {
+     void testAddEtudiant() {
 
         Etudiant etudiant = new Etudiant();
         etudiant.setNomE("Doe");
@@ -39,14 +39,14 @@ class EtudiantServiceImplDynamiqueTest {
 
     @Order(2)
     @Test
-    public void testRetrieveAllEtudiants() {
+     void testRetrieveAllEtudiants() {
         List<Etudiant> etudiants = etudiantService.retrieveAllEtudiants();
         assertTrue(etudiants.size() > 0);
     }
 
     @Order(3)
     @Test
-    public void testRetrieveEtudiant() {
+     void testRetrieveEtudiant() {
         Etudiant etudiant = etudiantService.retrieveEtudiant(3);
         assertEquals(3, etudiant.getIdEtudiant());
     }
@@ -55,18 +55,24 @@ class EtudiantServiceImplDynamiqueTest {
 
     @Order(4)
     @Test
-    public void testUpdateEtudiant() {
-        Etudiant etudiant = etudiantRepository.findById(3).get();
-        etudiant.setNomE("Updated nom");
-        etudiant.setPrenomE("Updated prenom");
-        etudiantRepository.save(etudiant);
-        assertEquals(3, etudiant.getIdEtudiant());
-        assertEquals("Updated nom", etudiant.getNomE());
-        assertEquals("Updated prenom", etudiant.getPrenomE());
+     void testUpdateEtudiant() {
+
+        Etudiant etudiant = new Etudiant();
+        etudiant.setNomE("Doe");
+        etudiant.setPrenomE("John");
+        Etudiant e = etudiantService.addEtudiant(etudiant);
+
+
+        e.setNomE("Updated nom");
+        e.setPrenomE("Updated prenom");
+        Etudiant updatedetudiant = etudiantService.updateEtudiant(e);
+        Etudiant retrievedetudiant = etudiantRepository.findById(updatedetudiant.getIdEtudiant()).orElse(null);
+        assertNotNull(updatedetudiant);
+        assertEquals(updatedetudiant.getNomE(),retrievedetudiant.getNomE());
     }
 
     @Test
-    public void testRemoveEtudiant() {
+     void testRemoveEtudiant() {
        etudiantRepository.deleteById(4);
        assertFalse(etudiantRepository.existsById(4));
 
@@ -76,5 +82,5 @@ class EtudiantServiceImplDynamiqueTest {
 
 
 
-}*/
+}
 
